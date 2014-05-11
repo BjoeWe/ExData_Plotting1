a8365b1b0bb67df14e23bde08cda5b466b458662
BjoeWe <bjoern.weidemann@googlemail.com>
2014-05-11 11:51:02 +0200

    Update plot2.r

--- plot2.r
+++ plot2.r
@@ -3,8 +3,17 @@ setwd("~/Downloads/GetData")
 # Using SQL library
 library(sqldf)
 # Read required data into dataset
-dataset <- read.csv.sql("household_power_consumption.txt",sep=";", header=TRUE, na.strings = "?"
-                   sql="select * from file where Date = '2/2/2007' or Date = '1/2/2007'")
+file <- "household_power_consumption.txt"
+init <- read.table(file,header = TRUE,nrows=100,sep = ";")
+classes <- sapply(init,class)
+data <- read.csv2.sql(file,
+                      sql="select * from file where Date = '1/2/2007' or Date = '2/2/2007'",
+                      header=TRUE,
+                      sep=";",
+                      colClasses=classes,
+                      comment.char = "",
+                      na.strings = "?")
+
 ## set date & time as POSIX 
 dataset$DateTime <- strptime((paste(dataset$Date, dataset$Time, sep="-")), format="%d/%m/%Y-%H:%M:%S")
 # set global active power from factor to numeric
